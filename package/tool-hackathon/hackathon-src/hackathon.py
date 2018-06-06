33b935beec7dc9b29ac8b774cb1ed50a2d57ab21
Flavio Vella <flavio@dividiti.com>
2018-06-06 13:18:58 +0100

    add func for reading src code of the minimizer

--- package/tool-hackathon/hackathon-src/hackathon.py
+++ package/tool-hackathon/hackathon-src/hackathon.py
@@ -1,9 +1,13 @@
+import inspect
+
 def hello(): # this is the wrapper to scipy minimizer
     
     return "Have a good hackathon"
 
 
-
+def get_min_func_src_code():
+    lines = inspect.getsource(hello)
+    return lines
 
 
 
