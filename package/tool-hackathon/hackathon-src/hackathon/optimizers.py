28dafa757a9b84a75c4f3fbb48e36c24ea0ea91c
Oscar Higgott <oscar.higgott@me.com>
2018-06-12 15:55:42 +0100

    random sampling minimizer

--- package/tool-hackathon/hackathon-src/hackathon/optimizers.py
+++ package/tool-hackathon/hackathon-src/hackathon/optimizers.py
@@ -1,5 +1,6 @@
 from scipy.optimize import minimize
-
+import random
+import numpy as np
 
 def my_nelder_mead( func, x0, my_args=(), my_options=None ):    # a non-stochastic minimizer for comparison
 
@@ -12,12 +13,22 @@ def my_cobyla( func, x0, my_args=(), my_options=None ):         # another non-st
 
 
 def my_minimizer( func, x0, my_args=(), my_options=None ):      # your own take!
+	# Simple optimiser: samples randomly and returns the minimum
+	num_parameters = len(x0)                                        # get number of parameters needed for objective function
+	points = 10                                                 	# evaluate the objective function at 10 points
+	flist = []
+	xlist = []
+	for i in range(points):
+		x = [random.random() for i in range(num_parameters)]    # initialise parameters randomly from uniform distribution
+		fnew = func(x, my_args)
+		flist.append(fnew)
+		xlist.append(x)
+	minidx = np.argmin(flist)
+	best_x = xlist[minidx]
+	fmin = flist[minidx]
+	minimizer_output = { 'fun' : fmin, 'nfev' : points, 'nit' : points, 'x' : best_x }
 
-    # Currently it does not optimize anything, just returns the input
-
-    minimizer_output = { 'fun' : func(x0), 'nfev' : 1 }
-    
-    return minimizer_output
+	return minimizer_output
 
 
 
