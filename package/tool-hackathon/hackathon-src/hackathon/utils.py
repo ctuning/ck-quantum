b31ac434c02f422834734daf66d95c8dcf58da82
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-06-18 15:10:34 +0100

    return all the results and control verbosity

--- package/tool-hackathon/hackathon-src/hackathon/utils.py
+++ package/tool-hackathon/hackathon-src/hackathon/utils.py
@@ -100,10 +100,11 @@ def benchmark_code(vqe_entry, N = 100, solution = 0., delta = 1e-1, p=0.95):
     return Tave, Terr, t_ave, t_err, s, s_err, out_list, n_samples_list
 
 
-def benchmark_list_of_runs(list_of_runs, delta, prob, which_fun_key, which_time_key, show_more=False):
+def benchmark_list_of_runs(list_of_runs, delta, prob, which_fun_key, which_time_key, show_more=False, verbose=True):
     "Perform benchmarking on the already collected JSON file from an experiment: CK entry"
 
-    print("benchmark_list_of_runs:  delta={}, prob={}, which_fun_key={}, which_time_key={}\n".format(delta, prob, which_fun_key, which_time_key))
+    if verbose:
+        print("benchmark_list_of_runs:  delta={}, prob={}, which_fun_key={}, which_time_key={}\n".format(delta, prob, which_fun_key, which_time_key))
 
     num_repetitions = len(list_of_runs)
     if num_repetitions:
@@ -115,7 +116,8 @@ def benchmark_list_of_runs(list_of_runs, delta, prob, which_fun_key, which_time_
         n_succ              = 0
         list_selected_times = []
 
-        print("experiment_file: Goal={}, Minimizer={}, Source:\n{}\n\n{}\n{}\n".format(classical_energy, minimizer_method, '-'*100, minimizer_src, '='*100))
+        if verbose:
+            print("experiment_file: Goal={}, Minimizer={}, Source:\n{}\n\n{}\n{}\n".format(classical_energy, minimizer_method, '-'*100, minimizer_src, '='*100))
 
         for run in list_of_runs:
             vqe_output      = run['vqe_output']
@@ -130,7 +132,8 @@ def benchmark_list_of_runs(list_of_runs, delta, prob, which_fun_key, which_time_
             q_shots         = report['total_q_shots']
             time_selected   = report[which_time_key]
 
-            print("fun_participant={:.4f}, fun_validated={:.4f}, fun_exact={:.4f}, Qtime={:.2f}sec, Qshots={}".format(fun, fun_validated, fun_exact, q_seconds, q_shots))
+            if verbose:
+                print("fun_participant={:.4f}, fun_validated={:.4f}, fun_exact={:.4f}, Qtime={:.2f}sec, Qshots={}".format(fun, fun_validated, fun_exact, q_seconds, q_shots))
 
             if abs( fun_selected - classical_energy )<delta:
                 n_succ += 1
@@ -138,10 +141,13 @@ def benchmark_list_of_runs(list_of_runs, delta, prob, which_fun_key, which_time_
             list_selected_times.append(time_selected)
 
         Tave, Terr, t_ave, t_err, s, s_err = total_time(list_selected_times, n_succ, num_repetitions, prob)
-        if show_more:
-            print("\nn_succ={}, Tave={:.4f}, Terr={:.4f}, t_ave={:.4f}, t_err={:.4f}, s={:.4f}, s_err={:.4f}\n\n".format(n_succ, Tave, Terr, t_ave, t_err, s, s_err))
-        else:
-            print("\nn_succ={}, Tave={:.4f}\n\n".format(n_succ, Tave))
+        if verbose:
+            if show_more:
+                print("\nn_succ={}, Tave={:.4f}, Terr={:.4f}, t_ave={:.4f}, t_err={:.4f}, s={:.4f}, s_err={:.4f}\n\n".format(n_succ, Tave, Terr, t_ave, t_err, s, s_err))
+            else:
+                print("\nn_succ={}, Tave={:.4f}\n\n".format(n_succ, Tave))
+
+        return classical_energy, minimizer_method, minimizer_src, n_succ, Tave, Terr, t_ave, t_err, s, s_err
 
 
 def get_min_func_src_code():
