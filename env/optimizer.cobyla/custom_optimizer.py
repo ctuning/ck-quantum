b2360acb044106fe75085a338c70bcab6492650d
Leo Gordon <ens-lg4@users.noreply.github.com>
2018-09-19 09:47:41 +0100

    removed the preinstalled env:optimizers as confusing (since we have deployables now)

--- env/optimizer.cobyla/custom_optimizer.py
+++
@@ -1,7 +0,0 @@
-import scipy.optimize
-
-
-def my_cobyla( func, x0, my_args=(), my_options=None ):
-    "Stock COBYLA optimizer from SciPy library"
-
-    return scipy.optimize.minimize( func, x0, method='COBYLA', options=my_options, args=my_args )
